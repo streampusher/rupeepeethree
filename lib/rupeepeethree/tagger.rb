23a1d7d8f76222fac6e4a56e478d9fe92ebb5acb
Tony Miller <mcfiredrill@gmail.com>
2013-03-13 22:58:37 -0700

    create apic frame if it doesnt exist

--- lib/rupeepeethree/tagger.rb
+++ lib/rupeepeethree/tagger.rb
@@ -3,7 +3,7 @@ require 'taglib'
 class Tagger
   def self.tag(mp3,tags)
     TagLib::MPEG::File.open(mp3) do |f|
-      t = f.id3v2_tag
+      t = f.id3v2_tag || TagLib::ID3v2::Tag.new
       if tags[:title]
         t.title = tags[:title]
       end
@@ -17,8 +17,28 @@ class Tagger
         t.year = tags[:year].to_i
       end
       if tags[:picture]
-        cover = t.frame_list('APIC').first
-        cover.picture = File.open((File.expand_path(tags[:picture])),"rb"){|f|f.read}
+        if !t.frame_list('APIC').first
+          cover = TagLib::ID3v2::AttachedPictureFrame.new
+          cover.type = TagLib::ID3v2::AttachedPictureFrame::FrontCover
+          cover.picture = File.open((File.expand_path(tags[:picture])),"rb"){|f|f.read}
+          t.add_frame(cover)
+        else
+          cover = t.frame_list('APIC').first
+          cover.type = TagLib::ID3v2::AttachedPictureFrame::FrontCover
+          cover.picture = File.open((File.expand_path(tags[:picture])),"rb"){|f|f.read}
+          t.add_frame(cover)
+        end
+      end
+      f.save
+    end
+  end
+
+  # clear all tags
+  def self.clear(mp3)
+    TagLib::MPEG::File.open(mp3) do |f|
+      tag = f.id3v2_tag
+      if tag
+        tag.remove_frames('TPE1')
       end
       f.save
     end
